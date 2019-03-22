f6560b71cd08a2f14a5b35d0abfeacdce81c8216
fzellini <fzellini@users.noreply.github.com>
2019-03-22 17:52:14 +0100

    Add files via upload

--- timvoip.py
+++ timvoip.py
@@ -9,13 +9,18 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='TIM SIP proxy IP address extractor')
     parser.add_argument("--verbose",action='store_true')
-    parser.add_argument("--nameserver",help="nameserver",default="85.37.17.58",required=False)
+    parser.add_argument("--nameserver",help="nameserver",required=False)
     parser.add_argument("--sip",help="sip proxy",required=True)
 
     args = parser.parse_args()
 
-    resolver = dns.resolver.Resolver(configure=False)
-    resolver.nameservers = [args.nameserver]
+    if args.nameserver is None:
+      resolver = dns.resolver.Resolver(configure=True)
+    else:
+      resolver = dns.resolver.Resolver(configure=False)
+      resolver.nameservers = [args.nameserver]
+
+
     srv_rec =  resolver.query("_sip._udp."+args.sip,'SRV')
 
     for srv in srv_rec:
